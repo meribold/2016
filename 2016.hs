d0172997826fac9481771fd31a620fada70a0bfb
Lukas Waymann <meribold@gmail.com>
2016-09-10 17:51:53 +0200

    Initial commit

---
+++ 2016.hs
@@ -0,0 +1,25 @@
+data SolutionCandidate = SolutionCandidate { nums :: [Double] , ops :: [Char] }
+   deriving(Show)
+
+problem = SolutionCandidate [1..10] []
+
+-- We can't generally convert functions to strings in Haskell; store the names
+-- along with the operators.
+allowedOps = [((+), '+'), ((-), '-'), ((*), '*'), ((/), '/')]
+
+-- Apply all operators to the first two numbers of a SolutionCandidate and
+-- return a list of the resulting new candidates.
+applyOps (SolutionCandidate (x:y:zs) ops) =
+   map (\(f, name) -> SolutionCandidate ((f x y):zs) (ops ++ [name])) allowedOps
+
+getSolutions :: SolutionCandidate -> [SolutionCandidate]
+-- Did we get 2016?  If so, just return the identity.
+getSolutions sC@(SolutionCandidate [2016] _) = [sC]
+-- If only one number remains in the list of numbers, return the empty list.
+getSolutions sC@(SolutionCandidate [x] _) = []
+-- Apply all operators to the first two numbers and recurse.
+getSolutions sC = concat $ map getSolutions (applyOps sC)
+
+main = print $ getSolutions problem
+
+-- vim: tw=80 sts=-1 sw=3 et
